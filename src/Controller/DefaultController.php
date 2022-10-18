ace73a7dad7331b27132b26f3df65221dfe853dd
konaten <konatenhamed@gmail.com>
2022-10-18 10:31:42 +0000

    dd

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_default')]
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig', [
+
+        ]);
+    }
+    #[Route('/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('_sections/index.html.twig', [
+
+        ]);
+    }
+}
